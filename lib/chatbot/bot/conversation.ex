19aea5cafbe5048fa893c3ad8338aea9103c176d
Mauricio <0253214@up.edu.mx>
2023-10-03 10:36:46 -0600

    added bot conversation schema

---
+++ lib/chatbot/bot/conversation.ex
@@ -0,0 +1,17 @@
+defmodule Chatbot.Bot.Conversation do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "chatbot_conversations" do
+
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(conversation, attrs) do
+    conversation
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
